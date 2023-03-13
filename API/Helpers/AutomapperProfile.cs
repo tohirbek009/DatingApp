a2a2d5a6df857160452a2d554315c9061efff9cd
tohirbek009 <odilovtohirbek009@gmail.com>
2023-03-13 19:45:50 +0500

    End of section 8

---
+++ API/Helpers/AutomapperProfile.cs
@@ -0,0 +1,19 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, 
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
